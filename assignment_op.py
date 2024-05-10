e0b7f9af50fffb26183946dbc88075a07d16caf5
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-10 17:03:51 +0530

    Add files via upload

---
+++ assignment_op.py
@@ -0,0 +1,23 @@
+# assignment operators
+a = 10
+# addition assignment
+a+=5
+print("a+=5:",a)
+# subtraction assignment
+a-=5
+print("a-=5:",a)
+# multiplication assignment
+a*=5
+print("a*=5:",a)
+# division assignment
+a/=5
+print("a/=5:",a)
+# floor division assignment
+a//=5
+print("a//=5:",a)
+# remainder assignment
+a%=5
+print("a%=5:",a)
+# exponent assignment
+a**=5
+print("a**=5:",a)
